73ad38b492381da41c885c4320293135023d32e8
Ramiro <rhuayller@agetic.gob.bo>
2017-07-03 14:37:58 -0400

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,64 @@
 <template>
-  <v-app id="app">
-    <v-navigation-drawer persistent v-model="drawer" light enable-resize-watcher>
-      <v-list dense>
+ <v-app id="app">
+  <div class="text-xs-center">
+    <v-btn dark v-bind:ripple="{ class: 'success--text' }">Colored Ripple</v-btn>
+  </div>
+    <v-navigation-drawer temporary v-model="drawer" :mini-variant.sync="mini" light>
+      <v-list class="pa-0">
         <v-list-item>
-          <v-list-tile @click.native.stop="left = !left">
+          <v-list-tile avatar tag="div">
+            <v-list-tile-avatar>
+              <img src="https://randomuser.me/api/portraits/men/85.jpg" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>John Leider</v-list-tile-title>
+            </v-list-tile-content>
             <v-list-tile-action>
-              <v-icon>exit_to_app</v-icon>
+              <v-btn icon dark @click.native.stop="mini = !mini">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list-item>
+      </v-list>
+      <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-item v-for="item in items" :key="item">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
-              <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-navigation-drawer persistent v-model="drawerRight" light right light enable-resize-watcher>
-      <v-list dense>
-        <v-list-item>
-          <v-list-tile @click.native.stop="right = !right">
-            <v-list-tile-action>
-              <v-icon>exit_to_app</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>Open Temporary Drawer</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar class="indigo" light>
+    <v-toolbar fixed class="deep-orange" light>
       <v-toolbar-side-icon light @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Toolbar</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-side-icon light @click.native.stop="drawerRight = !drawerRight"></v-toolbar-side-icon>
     </v-toolbar>
     <main>
-      <v-navigation-drawer
-        temporary
-        v-model="left"
-      ></v-navigation-drawer>
       <v-container fluid>
+        <div class="title">Click on sidebar to re-open.</div>
         <!--v-router-->
         <router-view></router-view>
       </v-container>
-      <v-navigation-drawer
-        right
-        temporary
-        v-model="right"
-      ></v-navigation-drawer>
     </main>
-    <v-footer class="indigo">
-      <span>© 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+     drawer: null,
+        items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ],
+        mini: false,
+        right: null
 }
 </script>
 
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,89 @@
 <template>
   <div class="hello">
 
+
+
+ <v-tabs id="mobile-tabs-3" grow light>
+    <v-tabs-bar slot="activators">
+      <v-tabs-slider></v-tabs-slider>
+      <v-tabs-item
+        v-for="i in 3"
+        :key="i"
+        :href="'#mobile-tabs-3-' + i"
+      >
+        Item {{ i }}
+      </v-tabs-item>
+    </v-tabs-bar>
+    <v-tabs-content
+      v-for="i in 3"
+      :key="i"
+      :id="'mobile-tabs-3-' + i"
+    >
+      <v-card flat>
+        <v-card-text>{{ text }}</v-card-text>
+      </v-card>
+    </v-tabs-content>
+  </v-tabs>
+
+
+<v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step step="1" :complete="e1 > 1">Name of step 1</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="2" :complete="e1 > 2">Name of step 2</v-stepper-step>
+      <v-divider></v-divider>
+      <v-stepper-step step="3">Name of step 3</v-stepper-step>
+    </v-stepper-header>
+    <v-stepper-content step="1">
+      <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px"></v-card>
+      <v-btn primary @click.native="e1 = 2" light>Continue</v-btn>
+      <v-btn flat dark>Cancel</v-btn>
+    </v-stepper-content>
+    <v-stepper-content step="2">
+      <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px"></v-card>
+      <v-btn primary @click.native="e1 = 3" light>Continue</v-btn>
+      <v-btn flat dark>Cancel</v-btn>
+    </v-stepper-content>
+    <v-stepper-content step="3">
+      <v-card class="grey lighten-1 z-depth-1 mb-5" height="200px"></v-card>
+      <v-btn primary @click.native="e1 = 1" light>Continue</v-btn>
+      <v-btn flat dark>Cancel</v-btn>
+    </v-stepper-content>
+  </v-stepper>
+
+
+ <v-card class="grey lighten-4 elevation-0">
+    <v-card-text>
+      <v-container fluid>
+        <v-layout row wrap>
+          <v-flex xs12 sm6>
+            <v-subheader v-text="'Multiple (Chips) with persistent hint'"></v-subheader>
+          </v-flex>
+          <v-flex xs12 sm6>
+            <v-select
+              label="Select"
+              :items="states"
+              multiple
+              chips
+              dark
+              hint="What are the target regions"
+              persistent-hint
+            ></v-select>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card-text>
+  </v-card>
+
+   <v-progress-linear v-bind:indeterminate="true"></v-progress-linear>
+  <v-parallax src="http://oxygennacdn3.oxygenna.com/wp-content/uploads/2015/11/18.jpg" height="300">
+   <v-layout column align-center justify-center>
+      <h1 class="white--text">Vuetify.js</h1>
+      <h4 class="white--text">Build your application today!</h4>
+    </v-layout>
+  </v-parallax>
+
+
 <v-layout justify-space-around>
     <v-icon large class="green--text text--darken-2">business</v-icon>
 
@@ -121,7 +204,27 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      users: []
+      users: [],
+       e1: 0,
+e6: [],
+        e7: [],
+        states: [
+          'Alabama','Alaska','American Samoa','Arizona',
+          'Arkansas','California','Colorado','Connecticut',
+          'Delaware','District of Columbia','Federated States of Micronesia',
+          'Florida','Georgia','Guam','Hawaii','Idaho',
+          'Illinois','Indiana','Iowa','Kansas','Kentucky',
+          'Louisiana','Maine','Marshall Islands','Maryland',
+          'Massachusetts','Michigan','Minnesota','Mississippi',
+          'Missouri','Montana','Nebraska','Nevada',
+          'New Hampshire','New Jersey','New Mexico','New York',
+          'North Carolina','North Dakota','Northern Mariana Islands','Ohio',
+          'Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico',
+          'Rhode Island','South Carolina','South Dakota','Tennessee',
+          'Texas','Utah','Vermont','Virgin Island','Virginia',
+          'Washington','West Virginia','Wisconsin','Wyoming'
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
     }
   }, 
   mounted(){
